5f0ed6b1a755a9a926883d3ef96dc8d83522c803
Vašek Chalupníček <vasek.chalupnicek@gmail.com>
2022-08-02 22:18:34 +0200

    version bump

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="micropython-octopuslab-installer",
-    version="0.2.1",
+    version="0.2.2",
     author="OctopusLAB",
     author_email="info@octopuslab.cz",
     description="OctopusLab installer for MicroPython",
